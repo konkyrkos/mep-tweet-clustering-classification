2a45263347e0294d5b5689b8d99350e77767081b
konkyrkos <kon.kyrkos@gmail.com>
2020-08-24 13:07:46 +0300

    add project files

---
+++ twitter_config.py
@@ -0,0 +1,7 @@
+config = {
+    'consumer_key': 'KuAjztJl8vexggJb6aNOMoD0W',
+    'consumer_secret': '1i93XzOjAVXzsRJ1TwSxU0lyeYG7UKLikGnU5H4MqqBAHq3Ihg',
+
+    'access_token': '3295115832-MjG7HPeqt1mh4HAZKFWZrQUZgifcJmschdYpycW',
+    'access_token_secret': 'W9ct86KvrIZCCzQWabX1PDxD8VMSNfXsurDjVKVnJUge1'
+}
